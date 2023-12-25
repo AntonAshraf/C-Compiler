0d196bc214b04f686142059128c7faa4803d7437
Anton Ashraf <anton0ashraf@gmail.com>
2023-12-25 06:25:48 +0200

    add LR Parser

--- Working/shift_reduce_meduim.c
+++ Working/shift_reduce_meduim.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 
@@ -6,75 +5,33 @@ struct ProductionRule
 {
     char left[100];
     char right[100];
-};
+} Rules[100];
+
+int ReadRules();
 
 int main()
 {
     char input[200], stack[500], temp[500], ch[20], *token1, *token2, *substring;
-    int i, j, stack_length, substring_length, stack_top, rule_count = 0;
-    struct ProductionRule rules[10];
+    int i, j, stack_length, substring_length, stack_top, rule_count;
 
     stack[0] = '\0';
 
-    // User input for the number of production rules
-    printf("\nEnter the number of production rules: ");
-    scanf("%d", &rule_count);
-
-    // User input for each production rule in the form 'left->right'
-    printf("\nEnter the production rules (in the form 'left->right'): \n");
-    for (i = 0; i < rule_count; i++)
-    {
-        scanf("%s", temp);
-        token1 = strtok(temp, "->");
-        token2 = strtok(NULL, "->");
-        strcpy(rules[i].left, token1);
-        strcpy(rules[i].right, token2);
-    }
-
+    rule_count = ReadRules();
     // User input for the input string
     printf("\nEnter the input string: ");
-    scanf("%s", input);
-/* 
-Statement->while(Condition)Block
-
-Condition-> id opertor digit
-Condition-> id opertor id
-Condition-> digit opertor id
-Condition-> digit opertor digit
-
-operator-><
-operator->>
-operator-><=
-operator->>=
-operator->==
-operator->!=
-
-digit->0
-digit->1
-digit->2
-digit->3
-digit->4
-digit->5
-digit->6
-digit->7
-digit->8
-digit->9
-
-id->idid
-
-
-Block->{Stat}
-
-Stat->StatStat
-Stat->id++;
-
-
- */
-
+    scanf("%[^\n]s", input);
 
     i = 0;
     while (1)
     {
+        if (i == 0)
+        {
+            printf("\nStack\tInput\tAction\n");
+        }
+        // if(input[i] == ' '){
+        //     i++;
+        //     continue;
+        // }
         // If there are more characters in the input string, add the next character to the stack
         if (i < strlen(input))
         {
@@ -89,12 +46,13 @@ Stat->id++;
             }
             printf("\tShift %s\n", ch);
         }
-        
+
         // Iterate through the production rules
         for (j = 0; j < rule_count; j++)
         {
             // Check if the right-hand side of the production rule matches a substring in the stack
-            substring = strstr(stack, rules[j].right);
+            substring = strstr(stack, Rules[j].right);
+            // printf("Stack: %s, Rule: |%s|, Substring: |%s|\n", stack, Rules[j].right, substring);
             if (substring != NULL)
             {
                 // Replace the matched substring with the left-hand side of the production rule
@@ -102,19 +60,19 @@ Stat->id++;
                 substring_length = strlen(substring);
                 stack_top = stack_length - substring_length;
                 stack[stack_top] = '\0';
-                strcat(stack, rules[j].left);
+                strcat(stack, Rules[j].left);
                 printf("%s\t", stack);
                 for (int k = i; k < strlen(input); k++)
                 {
                     printf("%c", input[k]);
                 }
-                printf("\tReduce %s->%s\n", rules[j].left, rules[j].right);
+                printf("\tReduce %s->%s\n", Rules[j].left, Rules[j].right);
                 j = -1; // Restart the loop to ensure immediate reduction of the newly derived production rule
             }
         }
 
         // Check if the stack contains only the start symbol and if the entire input string has been processed
-        if (strcmp(stack, rules[0].left) == 0 && i == strlen(input))
+        if (strcmp(stack, Rules[0].left) == 0 && i == strlen(input))
         {
             printf("\nAccepted");
             break;
@@ -129,4 +87,59 @@ Stat->id++;
     }
 
     return 0;
+}
+
+int ReadRules()
+{
+    const char *fileName = "rules.txt";
+
+    FILE *file = fopen(fileName, "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error opening file: %s\n", fileName);
+        return 1;
+    }
+    printf("File opened successfully\n");
+    int ruleIndex = 0;
+    char line[256];
+    while (fgets(line, sizeof(line), file))
+    {
+        // Skip empty lines or lines starting with '\n'
+        if (line[0] == '\n' || line[0] == '\0')
+            continue;
+
+        char *arrowPos = strstr(line, "->");
+        if (!arrowPos)
+        {
+            fprintf(stderr, "Invalid rule: %s\n", line);
+            continue;
+        }
+
+        *arrowPos = '\0'; // Separate left and right sides
+        char *left = line;
+        char *right = arrowPos + 2;
+
+        size_t len = strlen(right);
+        if (len > 0 && right[len - 1] == '\n')
+        {
+            right[len - 1] = '\0'; // Remove the trailing '\n'
+        }
+
+        strncpy(Rules[ruleIndex].left, left, sizeof(Rules[ruleIndex].left) - 1);
+        Rules[ruleIndex].left[sizeof(Rules[ruleIndex].left) - 1] = '\0';
+
+        strncpy(Rules[ruleIndex].right, right, sizeof(Rules[ruleIndex].right) - 1);
+        Rules[ruleIndex].right[sizeof(Rules[ruleIndex].right) - 1] = '\0';
+
+        ruleIndex++;
+    }
+
+    fclose(file);
+
+    // Printing the rules for verification
+    for (int i = 0; i < ruleIndex; ++i)
+    {
+        printf("Rule %d: |%s| -> |%s|\n", i + 1, Rules[i].left, Rules[i].right);
+    }
+    return ruleIndex;
 }
