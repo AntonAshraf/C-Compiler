a5c2183164e393731f8bbc983d0301d23a3749c4
Omar Ahmed <omarahmed170@gmail.com>
2023-12-25 10:15:10 +0200

    Refactor while loop in While.c

--- Compiler/SyntaxTree/While.c
+++ Compiler/SyntaxTree/While.c
@@ -1,11 +1,6 @@
-int main() {
-    int a = 10;
-    int b = 20;
-    
-    while (a < b) {
-        printf("a is less than b");
-        a++;
-    }
 
-    return 0;
+while (a < b)
+{
+    printf("a is less than b", x);
+    a++;
 }
